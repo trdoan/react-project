f400d5b125ffc3135bf9f2cfa3de0f2cd341c76a
trdoan <tranduongdoan0922@gmail.com>
2024-06-01 19:57:22 +0700

    feat: use `react-router-dom`

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { ApolloProvider } from '@apollo/client'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import { App } from './components/App'
 import { apolloClient } from './libs/apollo'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ApolloProvider client={apolloClient}>
-      <App />
-    </ApolloProvider>
+    <BrowserRouter>
+      <ApolloProvider client={apolloClient}>
+        <App />
+      </ApolloProvider>
+    </BrowserRouter>
   </StrictMode>,
 )
