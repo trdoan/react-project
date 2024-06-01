26f0dacfc6f4a8efe9f2c78b0dae04268309965b
trdoan <tranduongdoan0922@gmail.com>
2024-06-01 17:56:24 +0700

    chore: define env variables

---
+++ src/constants/env.ts
@@ -0,0 +1,3 @@
+export const env = {
+  REACT_APP_GRAPHQL_URI: import.meta.env.REACT_APP_GRAPHQL_URI,
+}
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env': process.env,
+  },
+  envPrefix: 'REACT_APP_',
   plugins: [react()],
 })
