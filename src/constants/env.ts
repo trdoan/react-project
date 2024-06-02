8b3164a1272a7390c96068eabcb1b951a6792978
trdoan <tranduongdoan0922@gmail.com>
2024-06-02 12:03:09 +0700

    refactor: update env variables

--- src/constants/env.ts
+++ src/constants/env.ts
@@ -1,3 +1,5 @@
 export const env = {
+  IS_PRODUCTION: import.meta.env.MODE === 'production',
+  REACT_APP_DISCORD_WEBHOOK: import.meta.env.REACT_APP_DISCORD_WEBHOOK,
   REACT_APP_GRAPHQL_URI: import.meta.env.REACT_APP_GRAPHQL_URI,
 }
