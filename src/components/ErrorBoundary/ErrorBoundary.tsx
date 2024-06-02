02591e5192a8c65e7ca3140262b4d6e33458cce9
trdoan <tranduongdoan0922@gmail.com>
2024-06-02 12:04:29 +0700

    feat: handle error global with `ErrorBoundary`

---
+++ src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -0,0 +1,52 @@
+/* eslint-disable react/require-default-props */
+/* eslint-disable no-console */
+import { logErrorToDiscordChannel } from '@/helpers/log'
+import { InternalServerError } from '@/pages'
+import React, { ErrorInfo, ReactNode } from 'react'
+
+interface ErrorBoundaryProps {
+  children: ReactNode
+  onError?: () => void
+}
+
+interface ErrorBoundaryState {
+  hasError: boolean
+}
+
+export class ErrorBoundary extends React.Component<
+  ErrorBoundaryProps,
+  ErrorBoundaryState
+> {
+  constructor(props: ErrorBoundaryProps) {
+    super(props)
+    this.state = { hasError: false }
+  }
+
+  static getDerivedStateFromError(_: Error): ErrorBoundaryState {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true }
+  }
+
+  componentDidMount(): void {
+    const { onError } = this.props
+    const { hasError } = this.state
+
+    if (hasError && onError) {
+      onError?.()
+    }
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo): void {
+    console.error({ error, errorInfo })
+    logErrorToDiscordChannel(error, errorInfo)
+  }
+
+  render() {
+    const { hasError } = this.state
+    const { children } = this.props
+
+    if (hasError) return <InternalServerError />
+
+    return !hasError && children
+  }
+}
