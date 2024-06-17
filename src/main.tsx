405da38e88d674a58cdb5c97c381abc8ba840ab5
trdoan <tranduongdoan0922@gmail.com>
2024-06-17 20:31:25 +0700

    refactor: remove unused and sorting imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,16 @@
+import { App } from '@/components/App'
+import { apolloClient } from '@/libs/apollo'
 import '@/styles/global.scss'
 import { ApolloProvider } from '@apollo/client'
-import { GoogleOAuthProvider } from '@react-oauth/google'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { App } from '@/components/App'
-import { env } from '@/constants/env'
-import { apolloClient } from '@/libs/apollo'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <ApolloProvider client={apolloClient}>
-        <GoogleOAuthProvider clientId={env.REACT_APP_GOOGLE_CLIENT_ID}>
-          <App />
-        </GoogleOAuthProvider>
+        <App />
       </ApolloProvider>
     </BrowserRouter>
   </StrictMode>,
